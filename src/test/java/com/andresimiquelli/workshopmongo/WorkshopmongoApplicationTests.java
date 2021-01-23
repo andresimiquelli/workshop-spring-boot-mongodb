970cdb31601a23443585220cec7b07c539a73218
André Simiquelli <andresimiquelli@hotmail.com>
2021-01-23 20:04:46 -0300

    Created project

---
+++ src/test/java/com/andresimiquelli/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
