950a62e38196a909e44dbc5b07ace062a7856992
André Simiquelli <andresimiquelli@hotmail.com>
2021-01-23 22:03:16 -0300

    User POST, PUT and DELETE

--- src/main/java/com/andresimiquelli/workshopmongo/services/UserService.java
+++ src/main/java/com/andresimiquelli/workshopmongo/services/UserService.java
@@ -7,21 +7,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andresimiquelli.workshopmongo.domain.User;
+import com.andresimiquelli.workshopmongo.dto.UserDTO;
 import com.andresimiquelli.workshopmongo.repository.UserRepository;
 import com.andresimiquelli.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = repository.findById(obj.getId()).get();
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());		
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	
 }
