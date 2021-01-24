204899b1664220c015854e10b07c0addb64ea9e3
André Simiquelli <andresimiquelli@hotmail.com>
2021-01-24 10:20:45 -0300

    Comments

--- src/main/java/com/andresimiquelli/workshopmongo/config/Instantiation.java
+++ src/main/java/com/andresimiquelli/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.andresimiquelli.workshopmongo.domain.Post;
 import com.andresimiquelli.workshopmongo.domain.User;
 import com.andresimiquelli.workshopmongo.dto.AuthorDTO;
+import com.andresimiquelli.workshopmongo.dto.CommentDTO;
 import com.andresimiquelli.workshopmongo.repository.PostRepository;
 import com.andresimiquelli.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post p1 = new Post(null, sdf.parse("21/08/2018"), "Partiu viagem", "Pessoal, estou indo pra Europa amanhã.", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("20/01/2019"), "Dicas de vigem no Brasil", "Gente, quero dicas de destinos no Brasil.", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!!!", sdf.parse("21/08/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Meu sonho é ir pra Europa. Aproveite!", sdf.parse("22/08/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Adoro Boipeba BA. Um dos lugares mais fantásticos do Mundo.", sdf.parse("20/01/2019"), new AuthorDTO(alex));
+		
+		p1.getComments().addAll(Arrays.asList(c1,c2));
+		p2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(p1,p2));
 		
